410ce0af266561c5c361a98d1cb3ea7a1b23cd3c
epthalite <fmy628.1998@icloud.com>
2022-01-15 18:47:42 +0900

    create form

--- front/app/pages/_app.tsx
+++ front/app/pages/_app.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
-import {AppProps} from 'next/app';
-import '../styles/globals.css';
+import React, { useState, useEffect, createContext } from "react"
+import { getCurrentUser } from "../lib/api/auth"
+import { User } from "../types"
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+// グローバルで扱う変数・関数
+export const AuthContext = createContext({} as {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  // 認証済みのユーザーがいるかどうかチェック
+  // 確認できた場合はそのユーザーの情報を取得
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log("No current user")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
+
+  // ユーザーが認証済みかどうかでルーティングを決定
+  // 未認証だった場合は「/signin」ページに促す
+  const Private = ({ children }: { children: React.ReactElement }) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children
+      } else {
+        return (<div></div>)
+      }
+    } else {
+      return <></>
+    }
+  }
+
+  return (
+<div>
+
+</div>
+  )
 }
 
-export default MyApp
+export default App
